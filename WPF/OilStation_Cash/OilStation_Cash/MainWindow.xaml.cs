28f75038dd3de496feb8796fae091a69f2d67b98
MurdexES <98350713+MurdexES@users.noreply.github.com>
2023-02-25 21:55:53 +0400

    Updates

--- WPF/OilStation_Cash/OilStation_Cash/MainWindow.xaml.cs
+++ WPF/OilStation_Cash/OilStation_Cash/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -27,14 +28,14 @@ namespace OilStation_Cash
             e.Handled = regex.IsMatch(e.Text);
         }
 
-    public MainWindow()
+        public MainWindow()
         {
             InitializeComponent();
         }
 
         private void CheckoutButton_Click(object sender, RoutedEventArgs e)
         {
-
+            TotalPriceText.Content = (Single.Parse(GasStationTotalText.Content.ToString()) + Single.Parse(CafeTotalText.Content.ToString())).ToString();
         }
 
         private void FuelComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -86,12 +87,84 @@ namespace OilStation_Cash
 
         private void AmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            if(AmountTextBox.Text != string.Empty) 
+            {
+                GasStationTotalText.Content = (Single.Parse(AmountTextBox.Text.ToString()) * Single.Parse(FuelPriceLabel.Content.ToString())).ToString();
+                PriceTextBox.Text = (Single.Parse(AmountTextBox.Text.ToString()) * Single.Parse(FuelPriceLabel.Content.ToString())).ToString();
+            }
         }
 
         private void PriceTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (PriceTextBox.Text != string.Empty)
+            {
+                GasStationTotalText.Content = PriceTextBox.Text;
+                AmountTextBox.Text = (Single.Parse(PriceTextBox.Text.ToString()) / Single.Parse(FuelPriceLabel.Content.ToString())).ToString();
+            }
+        }
 
+        private void HotDogCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            HotDogAmountTextBox.IsReadOnly = false;
+        }
+
+        private void HamburgerCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            HamburgerAmountTextBox.IsReadOnly = false;
+        }
+
+        private void FrenchFriesCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            FrenchFriesAmountTextBox.IsReadOnly = false;
+        }
+
+        private void CocaColaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            CocaColaAmountTextBox.IsReadOnly = false;
+        }
+
+        private void HotDogAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (HotDogAmountTextBox.Text != string.Empty)
+            {
+                var tmp_price = Double.Parse(HotDogAmountTextBox.Text) * 4;
+                var tmp_total = (Double)CafeTotalText.Content;
+
+                CafeTotalText.Content = (tmp_price + tmp_total).ToString();
+            }
+        }
+
+        private void HamburgerAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (HamburgerAmountTextBox.Text != string.Empty)
+            {
+                float tmp_price = Int32.Parse(HamburgerAmountTextBox.Text) * 5.4f;
+                float tmp_total = (Single)CafeTotalText.Content;
+
+                CafeTotalText.Content = (tmp_total + tmp_price).ToString();
+            }
+        }
+
+        private void FrenchFriesAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (FrenchFriesAmountTextBox.Text != string.Empty)
+            {
+                float tmp_price = Single.Parse(FrenchFriesAmountTextBox.Text) * 7.2f;
+                float tmp_total = Single.Parse(CafeTotalText.Content.ToString());
+
+                CafeTotalText.Content = (tmp_total + tmp_price).ToString();
+            }
+        }
+
+        private void CocaColaAmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (CocaColaAmountTextBox.Text != string.Empty)
+            {
+                float tmp_price = Single.Parse(CocaColaAmountTextBox.Text) * 4.4f;
+                float tmp_total = Single.Parse(CafeTotalText.Content.ToString());
+
+                CafeTotalText.Content = (tmp_total + tmp_price).ToString();
+            }
         }
     }
 }
