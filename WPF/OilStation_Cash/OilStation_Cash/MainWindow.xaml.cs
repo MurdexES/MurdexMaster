b535080d92bcaca014b8ee9719e9243fed6ff9a1
MurdexES <98350713+MurdexES@users.noreply.github.com>
2023-02-25 18:15:36 +0400

    GasStation Update

--- WPF/OilStation_Cash/OilStation_Cash/MainWindow.xaml.cs
+++ WPF/OilStation_Cash/OilStation_Cash/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +21,77 @@ namespace OilStation_Cash
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+    public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CheckoutButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FuelComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (((ComboBoxItem)FuelComboBox.SelectedItem).Content)
+            {
+                case "A-92":
+                    FuelPriceLabel.Content = "4.00";
+                    break;
+                case "A-94":
+                    FuelPriceLabel.Content = "4.50";
+                    break;
+                case "A-96":
+                    FuelPriceLabel.Content = "5.20";
+                    break;
+                case "Premium":
+                    FuelPriceLabel.Content = "6.00";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void FuelAmountRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if(FuelAmountRadioButton.IsChecked == true) 
+            {
+                AmountTextBox.IsReadOnly = false;
+                
+                PriceTextBox.IsReadOnly = true;
+                PriceTextBox.Text = string.Empty;
+
+                FuelPriceRadioButton.IsChecked = false;
+            }
+        }
+
+        private void FuelPriceRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if(FuelPriceRadioButton.IsChecked == true)
+            {
+                PriceTextBox.IsReadOnly = false;
+                
+                AmountTextBox.IsReadOnly = true;
+                AmountTextBox.Text = string.Empty;
+
+                FuelAmountRadioButton.IsChecked = false;
+            }
+        }
+
+        private void AmountTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PriceTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
