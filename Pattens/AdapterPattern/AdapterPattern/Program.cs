57ee3a0847367dd98bc91cb7c48604def91abc59
MurdexES <98350713+MurdexES@users.noreply.github.com>
2023-04-13 23:10:54 +0400

    Added AdapterPattern

---
+++ Pattens/AdapterPattern/AdapterPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("");
+
+IXmlService _xmlService = new JsonToXmlAdapter();
+_xmlService.Serialize("Message - Serialized using Adapter");
+
+
+interface IXmlService
+{
+    void Serialize(string message);
+}
+
+class JsonSerializer
+{
+    public void Serialize(string message)
+    {
+        Console.WriteLine("Json Serialized : " + message);
+    }
+}
+
+class JsonToXmlAdapter : IXmlService
+{
+    private JsonSerializer _jsonSerializer = new JsonSerializer();
+
+    public void Serialize(string message)
+    {
+        _jsonSerializer.Serialize(message);
+    }
+}
+
