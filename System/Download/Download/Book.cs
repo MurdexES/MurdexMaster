6ffd56d8c90931e8696869a761542285ab67a757
MurdexES <98350713+MurdexES@users.noreply.github.com>
2023-07-10 00:31:02 +0400

    DownloadHW Added

---
+++ System/Download/Download/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Download
+{
+    public class Book
+    {
+        public Book() { }
+
+        public Book(string title, string description, string author) 
+        {
+            Title = title;
+            Description = description;
+            Author = author;
+        }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ System/Download/Download/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Download
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Book> Books { get; set; } = new List<Book>
+        {
+            new Book("The Financier", "Financial book", "Theodore Dreiser"),
+            new Book("The Dark Tower", "Fiction Horror", "Stephen King")
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Download()
+        {
+            for (int i = 0; i < Books.Count; i++) 
+            {
+                Dispatcher.Invoke(() => BooksLB.Items.Add(Books[i]));
+            }
+        }
+
+        private void DownloadBT_Click(object sender, RoutedEventArgs e)
+        {
+            Thread thread = new(Download);
+            thread.Start();
+        }
+    }
+}
