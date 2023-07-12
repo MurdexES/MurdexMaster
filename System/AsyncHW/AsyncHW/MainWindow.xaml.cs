3d463f87be7838842b6dc0f15e4d82aa51ab2c7b
MurdexES <98350713+MurdexES@users.noreply.github.com>
2023-07-12 10:33:47 +0400

    Added AsyncHW

---
+++ System/AsyncHW/AsyncHW/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using AsyncHW.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncHW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly CountriesContext countryContext;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            countryContext = new CountriesContext();
+            LoadCountries();
+        }
+
+        private void LoadCountries()
+        {
+            CountriesLB.ItemsSource = countryContext.Countries.ToList();
+        }
+
+        public async Task CreateAsync(Country country)
+        {
+            using (var context = new CountriesContext())
+            {
+                context.Countries.Add(country);
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task DeleteAsync(Country country)
+        {
+            using (countryContext)
+            {
+                countryContext.Countries.Remove(country);
+                await countryContext.SaveChangesAsync();
+            }
+        }
+
+        private async void CreateBT_Click(object sender, RoutedEventArgs e)
+        {
+            string name;
+            uint yearFounded;
+            Country country;
+
+            if (CountryNameTB.Text != null && CountryYearTB.Text != string.Empty)
+            {
+                name = CountryNameTB.Text.ToString();
+                yearFounded = UInt32.Parse(CountryYearTB.Text.ToString());
+
+                country = new Country(name, yearFounded, string.Empty, string.Empty, 0, 0, 0);
+
+                await CreateAsync(country);
+            }
+            else
+            {
+                MessageBox.Show("No valid info for creation");
+            }
+        }
+
+        private async void DeleteBT_Click(object sender, RoutedEventArgs e)
+        {
+            Country country;
+
+            country = CountriesLB.SelectedItem as Country;
+
+            await DeleteAsync(country);
+        }
+    }
+}
---
+++ System/AsyncHW/AsyncHW/Migrations/20230712062030_Fix.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AsyncHW.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    YearFounded = table.Column<long>(type: "bigint", nullable: false),
+                    GovernmentType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MapImageLink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Population = table.Column<long>(type: "bigint", nullable: false),
+                    Area = table.Column<long>(type: "bigint", nullable: false),
+                    GDP = table.Column<double>(type: "float", nullable: false),
+                    CurrentRuler = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NationalAnthem = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
