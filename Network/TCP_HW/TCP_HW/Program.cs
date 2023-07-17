c9ed5f370a4f595a2903c574c12209ca1f58a836
MurdexES <98350713+MurdexES@users.noreply.github.com>
2023-07-17 12:25:02 +0400

    TCP_HW Added

---
+++ Network/TCP_HW/TCP_HW/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+string serverAddress = "127.0.0.1";
+int serverPort = 8080;
+
+TcpClient tcpClient = new TcpClient(serverAddress, serverPort);
+Console.WriteLine($"Client Connected Port {serverPort}");
+
+NetworkStream stream = tcpClient.GetStream();
+
+
+string request = "Hello Server";
+byte[] requestBytes = Encoding.UTF8.GetBytes(request);
+stream.Write(requestBytes, 0, requestBytes.Length);
+
+Console.WriteLine($"Sent message: {request}");
+
+
+byte[] responseBuffer = new byte[1024];
+int bytesRead = stream.Read(responseBuffer, 0, responseBuffer.Length);
+string response = Encoding.UTF8.GetString(responseBuffer, 0, bytesRead);
+
+Console.WriteLine($"Received message: {response}");
+
+tcpClient.Close();
---
+++ Network/TCP_HW/TCP_Listener/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+int port = 8080;
+
+TcpListener tcpListener = new TcpListener(ipAddress, port);
+
+tcpListener.Start();
+
+Console.WriteLine($"Server listening port {port}");
+
+try
+{
+    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+    Console.WriteLine("Client Connected");
+
+    NetworkStream stream = tcpClient.GetStream();
+
+    byte[] buffer = new byte[1024];
+    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+    string request = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+    Console.WriteLine($"Received message: {request}");
+
+    string response = "Server Heard You";
+    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
+    stream.Write(responseBytes, 0, responseBytes.Length);
+    Console.WriteLine($"Sent message: {response}");
+
+    tcpClient.Close();
+}
+finally
+{
+    tcpListener.Stop();
+}
