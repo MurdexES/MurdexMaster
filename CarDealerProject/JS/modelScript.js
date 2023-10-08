e6c7bb5eb93e888896611e3477fd1d0f0494bcb1
MurdexES <98350713+MurdexES@users.noreply.github.com>
2023-10-08 22:49:50 +0400

    CV Web added

--- CarDealerProject/JS/modelScript.js
+++ CarDealerProject/JS/modelScript.js
@@ -37,3 +37,33 @@ const scrollHeader = () =>{
                        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else  scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SHOW SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () => {
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+		}else{
+			document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW REVEAL ANIMATION ===============*/
