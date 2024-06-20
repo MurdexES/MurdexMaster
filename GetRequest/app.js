cddd102b57058aff48adf96ef50c55ae4458c905
MurdexES <98350713+MurdexES@users.noreply.github.com>
2024-06-21 00:02:26 +0400

    HW 1 fix

--- GetRequest/app.js
+++ GetRequest/app.js
@@ -3,14 +3,16 @@ const axios = require('axios');
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    try {
-        const response = axios.get('jsonplaceholder.typicode.com/users');
-        const users = response.data;
-        res.json(users);
+app.get('/', (req, res) => {
+    res.send('Users API');
+});
 
+app.get('/users', async (req, res) => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        res.json(response.data);
     } catch (error) {
-        res.status(500).json({ error : 'error' });
+        res.status(500).send('Error');
     }
 });
 
